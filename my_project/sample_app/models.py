48ebd9cbd311de29b4052ca9493d9411081c2e12
akazawaya <ktakimotondsinc.gmail@com>
2025-08-20 12:55:26 +0900

    アプリケーションsample_app の作成、 プロジェクトへの登録

---
+++ my_project/sample_app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+# CharFieldとは文字列を保存するためのフィールド
+# __str__(self) は特殊メソッド　オブジェクトを人間が読む文字列に変換
+
+class post(models.Model):
+    name = models.CharField(max_length=100)
+    micropost = models.CharField(max_length=140, blank=True)
+    
+    def __str__(self):
+        return self.name
