334d6c8a4e1d9215a8832a1ca974643440fb8701
akazawaya <ktakimotondsinc.gmail@com>
2025-08-20 17:56:57 +0900

    view(処理の定義）を作成中

--- my_project/sample_app/views.py
+++ my_project/sample_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+# この中にView（処理の定義）をすべて実装する
+"""
+create_post「作成」
+ ページロード時にはPostオブジェクトによりフォームを生成、実行ボタン押下時にはフォームに入力された値から、Postオブジェクトを生成。
+read_post「一覧表示」
+　データベースから全データを所得し、そのままtemplateへ渡す。
+edit_post「編集」
+　引数で渡されたidにより、Post オブジェクトを取得し、フォームを生成。
+delete_post「削除」
+　引数で渡されたidにより、Post オブジェクトを取得し、オブジェクトを削除。
+"""
+
+
