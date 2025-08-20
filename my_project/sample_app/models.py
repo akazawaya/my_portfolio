b5fe36e1b891e29c03ca348271b7aaa665bdb587
akazawaya <ktakimotondsinc@gmail.com>
2025-08-21 07:25:03 +0900

    タイポ修正

--- my_project/sample_app/models.py
+++ my_project/sample_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 # CharFieldとは文字列を保存するためのフィールド
 # __str__(self) は特殊メソッド　オブジェクトを人間が読む文字列に変換
 
-class post(models.Model):
+class Post(models.Model):
     name = models.CharField(max_length=100)
     micropost = models.CharField(max_length=140, blank=True)
     
