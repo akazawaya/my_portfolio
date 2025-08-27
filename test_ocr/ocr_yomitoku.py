898f22784f55c1ad6903c918a740dcc88e07c49b
akazawaya <ktakimotondsinc.gmail@com>
2025-08-27 14:15:36 +0900

    yomiyoku_test

--- test_ocr/ocr_yomitoku.py
+++ test_ocr/ocr_yomitoku.py
@@ -1,22 +1,37 @@
+# -*- coding: utf-8 -*-
 import cv2
-from yomitoku import LayoutAnalyzer
+import os
+from pathlib import Path
+from yomitoku import DocumentAnalyzer, OCR
 from yomitoku.data.functions import load_pdf
+
+BASE_DIR = Path(__file__).parent
+OUT_DIR = BASE_DIR / "results" / "yomitoku"
+
+PATH_IMG = "images/demo.jpg"
+ext = os.path.splitext(PATH_IMG)[1].lower()
+# .splitext で拡張子を所得（.付き）、lower で小文字化
+
+def load_image(path):
+    if ext == ".pdf":
+        imgs = load_pdf(path)
+    else:
+        img = cv2.imread(path)
+        imgs = [img]
+    return imgs
+
+
 if __name__ == "__main__":
+    configs = {
+        "text_detector":   {"path_cfg": str(BASE_DIR/"models"/"yomitoku"/"text_detector.yaml")},
+        "text_recognizer": {"path_cfg": str(BASE_DIR/"models"/"yomitoku"/"text_recognizer.yaml")},
+    }
 
-    analyzer = LayoutAnalyzer(visualize=True, device="cuda")
-
-    # PDFファイルを読み込み
-
-    imgs = load_pdf("demo/sample.pdf")
-
+    analyzer = DocumentAnalyzer(configs=configs, visualize=False, device="auto")
+    
+    imgs = load_image(PATH_IMG)
+    
     for i, img in enumerate(imgs):
-
-        results, layout_vis = analyzer(img)
-
-
-
-        # JSON形式で解析結果をエクスポート
-
-        results.to_json(f"output_{i}.json")
-
-        cv2.imwrite(f"output_layout_{i}.jpg", layout_vis)
+        results, _,_  = analyzer(img)
+        print(results.words[0].points)
+        results.to_json(f"{OUT_DIR}/output_{i}.json")
